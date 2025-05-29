743532c19e5039de7ed80704e721629425d824ae
Vaishnavi Nimbalkar <96813801+vaishnavinimbalkar@users.noreply.github.com>
2025-05-29 10:38:35 +0530

    [1] [Time Beats: 98.83%] [Memory Beats: 94.65%] - LeetPush

--- Two-Sum.java
+++ Two-Sum.java
@@ -1,19 +1,16 @@
-import java.util.*;
-import java.io.*;
-class Solution
-{
-    public int [] twoSum(int [] nums, int target )
-    {
-       for(int i=0; i<nums.length;i++)
-       {
-        for(int j=i+1; j<nums.length; j++)
-        {
-            if(nums[i]+nums[j]==target)
-            {
-               return new int [] {i,j};
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complements = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (complements.containsKey(nums[i])) {
+                return new int[]{complements.get(nums[i]), i};
             }
+            complements.put(target - nums[i], i);
         }
-       }
-    return new int[] {} ;
+        return null;
     }
 }
